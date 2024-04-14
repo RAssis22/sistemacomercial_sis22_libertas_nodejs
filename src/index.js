a532d378d138d043e5cdb819dfee5e619f0a0e21
RAssis22 <raageremias8@gmail.com>
2024-04-14 13:41:23 -0300

    inicio
    criação do compra.html, compra.js, compracontroller.js, compraroute.js,  e index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 const UsuarioRoute = require('./UsuarioRoute');
 const VendedorRoute = require('./VendedorRoute');
 const ProdutoRoute = require('./ProdutoRoute');
+const CompraRoute = require('./CompraRoute');
 module.exports = (app) => {
    UsuarioRoute(app)
    VendedorRoute(app)
    ProdutoRoute(app)
+   CompraRoute(app)
 }
